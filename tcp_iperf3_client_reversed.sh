fbc9991cfd88cb56f4a960bebcfe1325eed2072f
lordberre <jakerlock@gmail.com>
2016-11-28 16:32:14 +0100

    Version 0.93

--- tcp_iperf3_client_reversed.sh
+++ tcp_iperf3_client_reversed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 0.92
+# Version 0.93
 target="$(curl -s project-mayhem.se/probes/ip.txt)"
 count=$(( ( RANDOM % 9999 )  + 100 ))
 #logevent='"$(logger -p info)"'
@@ -11,7 +11,7 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
     sleep $[ ( $RANDOM % 240 )  + 60 ]s
     /bin/iperf3 --client $target -T Download -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "Timer stopped - tcp daemon started" | logger -p info 
      sleep 15
-     rrdtool updatev /home/chprobe/tcpdb.rrd --template downstream N:$(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') ## debug ## & echo $(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') >> /home/chprobe/dbdebug.log
+     rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template downstream N:$(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') ## debug ## & echo $(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') >> /home/chprobe/dbdebug.log
     ;;
 1)  echo "iperf tcp daemon running, all OK:" | logger -p info
     ;;
@@ -19,3 +19,4 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
     kill $(pgrep -f "iperf3 --client" | awk '{print $1}')
     ;;
 esac;
+
