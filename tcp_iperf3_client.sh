78d819706e2caa2426843b9a0ddcdca7e6dc0db8
lordberre <jakerlock@gmail.com>
2016-11-30 21:50:46 +0100

    Version 0.95 - backoff timer

--- tcp_iperf3_client.sh
+++ tcp_iperf3_client.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 0.93
+# Version 0.95
 target="$(curl -s project-mayhem.se/probes/ip.txt)"
 count=$(( ( RANDOM % 9999 )  + 100 ))
 #logevent='"$(logger -p info)"'
@@ -7,15 +7,15 @@ count=$(( ( RANDOM % 9999 )  + 100 ))
 # Uncomment to use Iperf TCP Daemon (with random timer)
 case "$(pgrep -f "iperf3 --client" | wc -w)" in
 
-0)  echo "iperf3 tcp daemon not running, initiating random timer then restarting daemon:" | logger -p info
-    sleep $[ ( $RANDOM % 240 )  + 60 ]s
-    /bin/iperf3 --client $target -T Upload -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "Timer stopped - tcp daemon started" | logger -p info 
+0)  echo "[chprobe_iperf3] iperf3 tcp daemon not running, initiating random timer then restarting daemon:" | logger -p info
+    while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 20 ) + 3]s  && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
+    /bin/iperf3 --client $target -T Upload -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] Timer stopped - tcp daemon started" | logger -p info
      sleep 15
      rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template upstream N:$(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') ## debug ## & echo $(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') >> /home/chprobe/dbdebug.log
     ;;
-1)  echo "iperf tcp daemon running, all OK:" | logger -p info
+1)  echo "[chprobe_iperf3] iperf tcp daemon running, all OK:" | logger -p info
     ;;
-*)  echo "multiple instances of iperf udp daemon running. Stopping & restarting iperf:" | logger -p info
+*)  echo "[chprobe_iperf3] multiple instances of iperf udp daemon running. Stopping & restarting iperf:" | logger -p info
     kill $(pgrep -f "iperf3 --client" | awk '{print $1}')
     ;;
 esac;
