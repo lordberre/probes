b4d30beaeffbc75384d62c325a8de92d0a043473
lordberre <jakerlock@gmail.com>
2016-12-05 21:23:28 +0100

    Update udp_iperf3_client.sh

--- udp_iperf3_client.sh
+++ udp_iperf3_client.sh
@@ -13,14 +13,14 @@ rand=$[ $RANDOM % 3 ]
 case "$(pgrep -f "iperf3 --client" | wc -w)" in
 
 0)  echo "[chprobe_iperf3] Let's see if we can start the udp daemon" | logger -p info
-    while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 10 ) + 3]s && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
+    while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
     echo "[chprobe_iperf3] Starting the udp daemon - upstream" | logger -p info
     /usr/bin/iperf3 --client $target -u -T upstream -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$7,$8,$9,$10,$11,$12,$13}' | logger -t iperf3udp[$(echo $count)] -p local4.debug
     sleep 1
     rrdtool update /home/chprobe/udpdb-us_$(hostname -d).rrd  --template us_jitter:us_pl:us_bitrate N:$(tail /var/log/iperf3udp.log | egrep $count | egrep up | awk '{print $11}'):$(tail /var/log/iperf3udp.log | egrep $count | egrep up  | awk '{print $14}' | tr -d '(%)'):$(tail /var/log/iperf3udp.log | egrep $count | egrep up | awk '{print $9}')
     ;;
 1)  echo "[chprobe_iperf3] iperf daemon already running" | logger -p info
-    while pgrep -f "iperf3 --client" | wc -w | grep 1;do sleep $[ ( $RANDOM % 10 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
+    while pgrep -f "iperf3 --client" | wc -w | grep 1;do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
     echo "[chprobe_iperf3] Starting the udp daemon - upstream" | logger -p info
     /usr/bin/iperf3 --client $target -u -T upstream -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$7,$8,$9,$10,$11,$12,$13}' | logger -t iperf3udp[$(echo $count)] -p local4.debug
     sleep 1
