c60b3e8e8ca11f5a5bdc01a66d7aadc73584848e
lordberre <jakerlock@gmail.com>
2016-12-05 20:41:27 +0100

    Version 0.6

--- udp_iperf3_client_reversed.sh
+++ udp_iperf3_client_reversed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 0.5
+# Version 0.6
 target="$(curl -s project-mayhem.se/probes/ip-udp.txt)"
 count=$(( ( RANDOM % 9999 )  + 1 ))
 iwnic=$(ifconfig | grep wl | awk '{print $1}' | tr -d ':')
@@ -12,14 +12,16 @@ rand=$[ $RANDOM % 3 ]
 # Uncomment to use Iperf UDP Daemon (with random timer)
 case "$(pgrep -f "iperf3 --client" | wc -w)" in
 
-0)  echo "[chprobe_iperf3] udp daemon started - downstream" | logger -p info
+0)  echo "[chprobe_iperf3] Let's see if we can start the udp daemon" | logger -p info
     while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 10 ) + 3]s && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
+    echo "[chprobe_iperf3] udp daemon started - downstream" | logger -p info
     /usr/bin/iperf3 --client $target -u -T downstream -R -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$7,$8,$9,$10,$11,$12,$13}' | logger -t iperf3udp[$(echo $count)] -p local4.debug
     sleep 1
     rrdtool update /home/chprobe/udpdb-ds_$(hostname -d).rrd --template ds_jitter:ds_pl:ds_bitrate N:$(tail /var/log/iperf3udp.log | egrep $count | egrep down | awk '{print $11}'):$(tail /var/log/iperf3udp.log | egrep $count | egrep down | awk '{print $14}' | tr -d '(%)'):$(tail /var/log/iperf3udp.log | egrep $count | egrep down | awk '{print $9}')
     ;;
 1)  echo "[chprobe_iperf3] iperf daemon already running" | logger -p info
     while pgrep -f "iperf3 --client" | wc -w | grep 1; do sleep $[ ( $RANDOM % 10 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
+    echo "[chprobe_iperf3] udp daemon started - downstream" | logger -p info   
     /usr/bin/iperf3 --client $target -u -T downstream -R -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$7,$8,$9,$10,$11,$12,$13}' | logger -t iperf3udp[$(echo $count)] -p local4.debug
     sleep 1
     rrdtool update /home/chprobe/udpdb-ds_$(hostname -d).rrd --template ds_jitter:ds_pl:ds_bitrate N:$(tail /var/log/iperf3udp.log | egrep $count | egrep down | awk '{print $11}'):$(tail /var/log/iperf3udp.log | egrep $count | egrep down | awk '{print $14}' | tr -d '(%)'):$(tail /var/log/iperf3udp.log | egrep $count | egrep down | awk '{print $9}')
