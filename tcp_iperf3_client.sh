3c3c6dc66617e4a46556a9a14aed17e2fa39f7fa
lordberre <jakerlock@gmail.com>
2016-12-05 20:43:31 +0100

    Fix minor timing issue

--- tcp_iperf3_client.sh
+++ tcp_iperf3_client.sh
@@ -8,7 +8,7 @@ count=$(( ( RANDOM % 9999 )  + 100 ))
 case "$(pgrep -f "iperf3 --client" | wc -w)" in
 
 0)  echo "[chprobe_iperf3] iperf3 tcp daemon not running, starting daemon" | logger -p info
-    while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 20 ) + 3]s  && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
+    while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 10 ) + 3]s  && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
     /bin/iperf3 --client $target -T Upload -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] tcp daemon started" | logger -p info
     sleep 5
     rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template upstream N:$(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') ## debug ## & echo $(tail /var/log/iperf3tcp.log | egrep $count | awk '{print $7}') >> /home/chprobe/dbdebug.log
