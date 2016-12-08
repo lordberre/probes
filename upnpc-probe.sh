747845cb882355c9638949a3492b8dd329ba3da5
lordberre <jakerlock@gmail.com>
2016-12-08 19:22:45 +0100

    Update upnpc-probe.sh

--- upnpc-probe.sh
+++ upnpc-probe.sh
@@ -1,6 +1,7 @@
 # Break the firewall
 # Only works with IPv4
 ip="$(hostname -I | awk '{print $1}')"
+echo -n "$(hostname -d) " > /home/chprobe/hostname-$(hostname -d) ;echo $(curl -s http://ping.eu | grep Your | awk '{print $4}' | tr -d '</b>') >> /home/chprobe/hostname-$(hostname -d)
 upnpc -e ch-probe -a $ip 5001 5001 tcp 1800
 upnpc -e ch-probe -a $ip 5001 5001 udp 1800
 upnpc -e ch-probe -a $ip 34521 34521 tcp 1800
