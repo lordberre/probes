7b53f2ce30f32bd1c49a1b08d924324c20560994
berre <berre@lordberre.lordberre>
2017-03-07 18:30:09 +0100

    modified:   udp_iperf3_client-logstash.sh 	modified:   udp_iperf3_client_reversed-logstash.sh

--- udp_iperf3_client-logstash.sh
+++ udp_iperf3_client-logstash.sh
@@ -16,12 +16,12 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
 0)  echo "[chprobe_iperf3] Let's see if we can start the udp daemon" | logger -p info
     while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
     echo "[chprobe_iperf3] Starting the udp daemon - $direction" | logger -p info
-    /usr/bin/iperf3 --client $target -u -T $direction -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & iw $iwnic link | sed -e "'s/^/$direction /'" | egrep "'$direction|flags|bitrate|signal'" | xargs | awk '{print $2,$6,$9,$10,$11}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
+    /usr/bin/iperf3 --client $target -u -T $direction -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & iw $iwnic link | sed -e "s/^/$direction /" | egrep "'$direction|flags|bitrate|signal'" | xargs | awk '{print $2,$6,$9,$10,$11}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
     ;;
 1)  echo "[chprobe_iperf3] iperf daemon already running" | logger -p info
     while pgrep -f "iperf3 --client" | wc -w | grep 1;do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
     echo "[chprobe_iperf3] Starting the udp daemon - $direction" | logger -p info
-        /usr/bin/iperf3 --client $target -u -T $direction -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & iw $iwnic link | sed -e "'s/^/$direction /'" | egrep "'$direction|flags|bitrate|signal'" | xargs | awk '{print $2,$6,$9,$10,$11}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
+        /usr/bin/iperf3 --client $target -u -T $direction -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & iw $iwnic link | sed -e "s/^/$direction /" | egrep "'$direction|flags|bitrate|signal'" | xargs | awk '{print $2,$6,$9,$10,$11}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
     ;;
 *)  echo "[chprobe_iperf3] multiple instances of iperf udp daemon running. Stopping & restarting iperf:" | logger -p info
     kill $(pgrep -f "iperf3 --client" | awk '{print $1}')
