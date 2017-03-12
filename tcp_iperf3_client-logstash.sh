e21363422bbf6415bc81d891ed76bb33553585db
berre <berre@lordberre.lordberre>
2017-03-12 01:19:37 +0100

    new file:   tcp_iperf3_client-logstash.sh 	new file:   tcp_iperf3_client_reversed-logstash.sh 	modified:   udp_iperf3_client-logstash.sh 	modified:   udp_iperf3_client_reversed-logstash.sh

---
+++ tcp_iperf3_client-logstash.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Version 0.961 (logstash)
+direction=upload
+target="$(curl -s project-mayhem.se/probes/ip.txt)"
+count=$(( ( RANDOM % 9999 )  + 100 ))
+
+### WiFi stuff
+iwnic=$(ifconfig | grep wl | awk '{print $1}' | tr -d ':')
+iwdetect="$(ifconfig | egrep wl | wc -l)"
+
+#logevent='"$(logger -p info)"'
+
+# Uncomment to use Iperf TCP Daemon (with random timer)
+case "$(pgrep -f "iperf3 --client" | wc -w)" in
+
+0)  echo "[chprobe_iperf3] Let's see if we can start the tcp daemon" | logger -p info
+    while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 5 ) + 3]s  && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
+    echo "[chprobe_iperf3] Starting the tcp daemon - $direction" | logger -p info
+    /bin/iperf3 --client $target -T $direction -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] tcp daemon started" | logger -p info & if [ $iwdetect -ge 0 ]; then
+    iw $iwnic link | egrep 'flags|bitrate|signal' | xargs | sed -e "s/^/$direction /" | awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local3.debug
+ else 'No WiFi NIC detected';fi
+    ;;
+1)  echo "[chprobe_iperf3] iperf tcp daemon is already running" | logger -p info
+    while pgrep -f "iperf3 --client" | wc -w | grep 1; do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
+    echo "[chprobe_iperf3] Starting the tcp daemon - $direction" | logger -p info
+    /bin/iperf3 --client $target -T $direction -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] Okey the daemon seems to be finished - starting our tcp daemon" | logger -p info & if [ $iwdetect -ge 0 ]; then
+    iw $iwnic link | egrep 'flags|bitrate|signal' | xargs | sed -e "s/^/$direction /" | awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local3.debug
+ else 'No WiFi NIC detected';fi
+;;
+*)  echo "[chprobe_iperf3] multiple instances of iperf udp daemon running. Stopping & restarting iperf:" | logger -p info
+    kill $(pgrep -f "iperf3 --client" | awk '{print $1}')
+    ;;
+esac;
--- udp_iperf3_client_reversed-logstash.sh
+++ udp_iperf3_client_reversed-logstash.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-# Version 0.6
+# Version 0.65
 direction="downstream"
 target="$(curl -s project-mayhem.se/probes/ip-wifi.txt)"
 count=$(( ( RANDOM % 9999 )  + 1 ))
+
+### WiFi stuff
 iwnic=$(ifconfig | grep wl | awk '{print $1}' | tr -d ':')
+iwdetect="$(ifconfig | egrep wl | wc -l)"
 # wifimetrics=$(awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz)
+###
+
 data=$(tail /var/log/iperf3udp.log | egrep $count | awk '{print $11,$14,$9}' | tr -d '(%)' | sed 's/ /,/g')
 arr[0]="5m"
 arr[1]="10m"
@@ -17,12 +22,16 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
 0)  echo "[chprobe_iperf3] Let's see if we can start the udp daemon" | logger -p info
     while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 10 ) + 3]s && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
     echo "[chprobe_iperf3] udp daemon started - $direction" | logger -p info
-    /usr/bin/iperf3 --client $target -u -T $direction -R -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & iw $iwnic link | egrep 'flags|bitrate|signal' | xargs | sed -e "s/^/$direction /" | awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
+    /usr/bin/iperf3 --client $target -u -T $direction -R -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & if [ $iwdetect -ge 0 ]; then
+    iw $iwnic link | egrep 'flags|bitrate|signal' | xargs | sed -e "s/^/$direction /" | awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
+ else 'No WiFi NIC detected';fi
     ;;
 1)  echo "[chprobe_iperf3] iperf daemon already running" | logger -p info
     while pgrep -f "iperf3 --client" | wc -w | grep 1; do sleep $[ ( $RANDOM % 10 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
     echo "[chprobe_iperf3] udp daemon started - $direction" | logger -p info   
-        /usr/bin/iperf3 --client $target -u -T $direction -R -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & iw $iwnic link | egrep 'flags|bitrate|signal' | xargs | sed -e "s/^/$direction /" | awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
+        /usr/bin/iperf3 --client $target -u -T $direction -R -b ${arr[$rand]} -t 60 | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print $1,$6,$8,$10,$13,$14.$15,$16,$17,$18}' | tr -d '(%)|:' | logger -t iperf3udp[$(echo $count)] -p local4.debug & if [ $iwdetect -ge 0 ]; then
+    iw $iwnic link | egrep 'flags|bitrate|signal' | xargs | sed -e "s/^/$direction /" | awk '{print $1,$3,$7,$10,$11,$12}' | tr -d MHz | logger -t linkstats[$(echo $count)] -p local4.debug
+ else 'No WiFi NIC detected';fi
     ;;
 *)  echo "[chprobe_iperf3] multiple instances of iperf udp daemon running. Stopping & restarting iperf:" | logger -p info
     kill $(pgrep -f "iperf3 --client" | awk '{print $1}')
