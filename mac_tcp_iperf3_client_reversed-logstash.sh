e7bfc3e55267de2a9fa278c7c935c0bd5f449c75
Test Labbet <jakerlock@gmail.com>
2017-03-18 18:50:17 +0100

    modified:   mac_tcp_iperf3_client-logstash.sh 	modified:   mac_tcp_iperf3_client_reversed-logstash.sh

--- mac_tcp_iperf3_client_reversed-logstash.sh
+++ mac_tcp_iperf3_client_reversed-logstash.sh
@@ -8,8 +8,11 @@ count=$(( ( RANDOM % 9999 )  + 100 ))
 phy=vht # What PHY?
 run_airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | egrep 'Rate|MCS|channel|agrCtlRSSI|CtlNoise' | tr -d 'agrCtlRSSI|agrCtlNoise|lastTxRate|maxRate|MCS|channel|:' | tr -d ' ' | sed 's/,/ /g' | xargs"
 maclogdir=/Users/testlabbet-pro/Labb-stuff/logz/iperf3tcp.log
-maclogformat='sed -e "s/^/$(date +%Y-%m-%d" %H:%M:%S") $(hostname -s) tx_airport_linkstats_$phy[$(echo $count)]: /"'
-maclogformat2='sed -e "s/^/$(date +%Y-%m-%d" %H:%M:%S") $(hostname -s) iperf3tcp[$(echo $count)]: /"'
+maclogformat='sed -e "s/^/$(date "+%b %d %H:%M:%S") $(hostname) tx_airport_linkstats_$phy[$(echo $count)]: /"'
+maclogformat2='sed -e "s/^/$(date "+%b %d %H:%M:%S") $(hostname) iperf3tcp[$(echo $count)]: /"'
+twentymhzformat='sed -e "s/^/$(date "+%b %d %H:%M:%S") $(hostname) tx_airport_linkstats[$(echo $count)]: /"'
+linkformat=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep channel | egrep '40|80' | wc -w | tr -d ' ')
+
 #iwnic=$(ifconfig | grep en0 | grep -v inet | awk '{print $1}' | tr -d ':')
 #iwdetect="$(ifconfig | egrep en0 | grep -v inet | wc -l)"
 
@@ -20,13 +23,13 @@ case $(pgrep -f "iperf3 --client" | wc -w | tr -d ' ') in
     while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 5 ) + 3]s  && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
     echo "[chprobe_iperf3] Starting the tcp daemon - $direction" | logger -p info
     /usr/local/bin/iperf3 --client $target -T $direction -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | eval $maclogformat2 >>$maclogdir & echo "[chprobe_iperf3] tcp daemon started" | logger -p info
-    eval $run_airport | sed -e "s/^/$direction /" | eval $maclogformat >> $maclogdir
-;;
+        eval $run_airport | sed -e "s/^/$direction /" | if [ $linkformat -ge 1 ]; then eval $maclogformat;else eval $twentymhzformat;fi >>$maclogdir
+    ;;
 1)  echo "[chprobe_iperf3] iperf tcp daemon is already running" | logger -p info
 	while pgrep -f "iperf3 --client" | wc -w | grep 1; do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
 	    echo "[chprobe_iperf3] Starting the tcp daemon - $direction" | logger -p info
 	        /usr/local/bin/iperf3 --client $target -T $direction -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | eval $maclogformat2 >>$maclogdir & echo "[chprobe_iperf3] tcp daemon started" | logger -p info
-        eval $run_airport | sed -e "s/^/$direction /" | eval $maclogformat >> $maclogdir
+		        eval $run_airport | sed -e "s/^/$direction /" | if [ $linkformat -ge 1 ]; then eval $maclogformat;else eval $twentymhzformat;fi >>$maclogdir
 		;;
 *)  echo "[chprobe_iperf3] multiple instances of iperf udp daemon running. Stopping & restarting iperf:" | logger -p info
     kill $(pgrep -f "iperf3 --client" | awk '{print $1}')
