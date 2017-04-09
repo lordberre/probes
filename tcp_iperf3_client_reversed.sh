d48a42fbbcfc880cfefdabf33e71fce5367fb1a7
lordberre <jakerlock@gmail.com>
2017-04-09 13:42:01 +0200

    modified:   tcp_iperf3_client.sh 	modified:   tcp_iperf3_client_reversed.sh

--- tcp_iperf3_client_reversed.sh
+++ tcp_iperf3_client_reversed.sh
@@ -11,6 +11,7 @@ phy=ht # What PHY?
 #phy=$1 # Use argument instead
 iwnic=$(ifconfig | grep wl | awk '{print $1}' | tr -d ':')
 iwdetect="$(grep up /sys/class/net/wl*/operstate | wc -l)"
+wififreq="$(iw $iwnic link | grep freq | awk '{print $2}')"
 
 #### HT TEMPLATE
 htparse="iw \$iwnic station dump | egrep 'tx bitrate|signal:' | xargs | sed 's/\[.*\]//' | tr -d 'short|GI' | sed 's/\<VHT-NSS\>//g' | sed -e \"s/^/\$direction /\" | awk '{print \$1,\$3,\$7,\$10,\$11,\$12,\$13}' | tr -d 'MHz' | logger -t tx_linkstats_\$phy[\$(echo \$count)] -p \$logfacility && iw \$iwnic station dump | egrep 'rx bitrate|signal:' | xargs | sed 's/\[.*\]//' | tr -d 'short|GI' | sed 's/\<VHT-NSS\>//g' | sed -e \"s/^/\$direction /\" | awk '{print \$1,\$3,\$7,\$10,\$11,\$12,\$13}' | tr -d 'MHz' | logger -t rx_linkstats_\$phy[\$(echo \$count)] -p \$logfacility && iw \$iwnic station dump | egrep 'bytes|packets|retries|failed' | xargs | tr -d 'rx|tx|bytes|packets|retries|failed:' | tr -s ' ' | logger -t iw_counters[\$(echo \$count)] -p \$logfacility"
@@ -28,18 +29,22 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
 0)  echo "[chprobe_iperf3] Let's see if we can start the tcp daemon" | logger -p info
     while iperf3 -c $target -t 1 | grep busy; do sleep $[ ( $RANDOM % 5 ) + 3]s  && echo '[chprobe_iperf3] waiting cuz server is busy' | logger -p info;done
     echo "[chprobe_iperf3] Starting the tcp daemon - $direction" | logger -p info
-    /bin/iperf3 --client $target -T $direction -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] tcp daemon started" | logger -p info & if [ $iwdetect -gt 0 ]; then
-if [ $phy = vht ]; then eval $vhtparse;else eval $htparse;fi
-else echo 'No WiFi NIC detected'>/dev/stdout;fi        
+    /bin/iperf3 --client $target -T $direction -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] tcp daemon started" | logger -p info & 
+    if [ $iwdetect -gt 0 ]; then
+	    if [ $wififreq -lt 2500 ]; then phy=ht & eval $htparse;else
+		    if [ $phy = vht ]; then eval $vhtparse;else eval $htparse;fi;fi
+	    else echo 'No WiFi NIC detected'>/dev/stdout;fi        
 sleep 15
     rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template $direction N:$(tail /var/log/iperf3tcp.log | egrep $count | grep iperf3 | awk '{print $7}')
 	;;
 1)  echo "[chprobe_iperf3] iperf tcp daemon is already running" | logger -p info
     while pgrep -f "iperf3 --client" | wc -w | grep 1; do sleep $[ ( $RANDOM % 5 ) + 3]s && echo '[chprobe_iperf3] waiting cuz a daemon is running' | logger -p info;done
     echo "[chprobe_iperf3] Starting the tcp daemon - $direction"
-    /bin/iperf3 --client $target -T $direction -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] Okey the daemon seems to be finished - starting our tcp daemon" | logger -p info & if [ $iwdetect -gt 0 ]; then
-if [ $phy = vht ]; then eval $vhtparse;else eval $htparse;fi
-else echo 'No WiFi NIC detected'>/dev/stdout;fi
+    /bin/iperf3 --client $target -T $direction -P 15 -R -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p local3.debug & echo "[chprobe_iperf3] Okey the daemon seems to be finished - starting our tcp daemon" | logger -p info &
+       if [ $iwdetect -gt 0 ]; then
+            if [ $wififreq -lt 2500 ]; then phy=ht & eval $htparse;else
+                    if [ $phy = vht ]; then eval $vhtparse;else eval $htparse;fi;fi
+            else echo 'No WiFi NIC detected'>/dev/stdout;fi 
 	sleep 15
     rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template $direction N:$(tail /var/log/iperf3tcp.log | egrep $count | grep iperf3 | awk '{print $7}')
    ;;
