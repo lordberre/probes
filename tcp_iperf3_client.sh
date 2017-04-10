8ec4c4f17569742cad06966083f55fb7f9011e0d
lordberre <jakerlock@gmail.com>
2017-04-10 20:13:48 +0200

    modified:   tcp_iperf3_client.sh 	modified:   tcp_iperf3_client_reversed.sh

--- tcp_iperf3_client.sh
+++ tcp_iperf3_client.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-# Version 0.963 (logstash)
+# Version 0.964
 direction=upstream
 logfacility=local3.debug
 target="$(curl -s project-mayhem.se/probes/ip.txt)"
 count=$(( ( RANDOM % 9999 )  + 100 ))
 
 ### WiFi stuff
-phy=ht # What PHY?
+iwnic=$(ifconfig | grep wl | awk '{print $1}' | tr -d ':') # Is there a wireless interface?
+iwdetect="$(grep up /sys/class/net/wl*/operstate | wc -l)" # Detect wireless interface state
+wififreq="$(iw $iwnic link | grep freq | awk '{print $2}')" # Detect frequency (2.4GHz or 5Ghz)
+phydetect="$(iw $iwnic link | grep VHT | wc -l)" # What PHY? (Legacy is not supported)
 #phy=$1 # Use argument instead
-iwnic=$(ifconfig | grep wl | awk '{print $1}' | tr -d ':')
-iwdetect="$(grep up /sys/class/net/wl*/operstate | wc -l)"
-wififreq="$(iw $iwnic link | grep freq | awk '{print $2}')"
 
 #### HT TEMPLATE
 htparse="iw \$iwnic station dump | egrep 'tx bitrate|signal:' | xargs | sed 's/\[.*\]//' | tr -d 'short|GI' | sed 's/\<VHT-NSS\>//g' | sed -e \"s/^/\$direction /\" | awk '{print \$1,\$3,\$7,\$10,\$11,\$12,\$13}' | tr -d 'MHz' | logger -t tx_linkstats_\$phy[\$(echo \$count)] -p \$logfacility && iw \$iwnic station dump | egrep 'rx bitrate|signal:' | xargs | sed 's/\[.*\]//' | tr -d 'short|GI' | sed 's/\<VHT-NSS\>//g' | sed -e \"s/^/\$direction /\" | awk '{print \$1,\$3,\$7,\$10,\$11,\$12,\$13}' | tr -d 'MHz' | logger -t rx_linkstats_\$phy[\$(echo \$count)] -p \$logfacility && iw \$iwnic station dump | egrep 'bytes|packets|retries|failed' | xargs | tr -d 'rx|tx|bytes|packets|retries|failed:' | tr -s ' ' | logger -t iw_counters[\$(echo \$count)] -p \$logfacility"
@@ -32,7 +32,7 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
     /bin/iperf3 --client $target -T $direction -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p $logfacility & echo "[chprobe_iperf3] tcp daemon started" | logger -p info &
         if [ $iwdetect -gt 0 ]; then
 	    if [ $wififreq -lt 2500 ]; then phy=ht & eval $htparse;else
-		    if [ $phy = vht ]; then eval $vhtparse;else eval $htparse;fi;fi
+                    if [ $phydetect -ge 1 ]; then phy=vht & eval $vhtparse;else phy=ht eval $htparse;fi;fi
 	    else echo 'No WiFi NIC detected'>/dev/stdout;fi      
     sleep 15
     rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template $direction N:$(tail /var/log/iperf3tcp.log | egrep $count | grep iperf3 | awk '{print $7}')    
@@ -43,7 +43,7 @@ case "$(pgrep -f "iperf3 --client" | wc -w)" in
     /bin/iperf3 --client $target -T $direction -P 15 -w 1m 2>&1 | egrep 'SUM.*rece' | awk '/Mbits\/sec/ {print $1,$7}' | tr -d ':' | logger -t iperf3tcp[$(echo $count)] -p $logfacility & echo "[chprobe_iperf3] Okey the daemon seems to be finished - starting our tcp daemon" | logger -p info &
         if [ $iwdetect -gt 0 ]; then
 	    if [ $wififreq -lt 2500 ]; then phy=ht & eval $htparse;else
-		    if [ $phy = vht ]; then eval $vhtparse;else eval $htparse;fi;fi
+                    if [ $phydetect -ge 1 ]; then phy=vht & eval $vhtparse;else phy=ht eval $htparse;fi;fi
 	    else echo 'No WiFi NIC detected'>/dev/stdout;fi      
     sleep 15
     rrdtool update /home/chprobe/tcpdb_$(hostname -d).rrd --template $direction N:$(tail /var/log/iperf3tcp.log | egrep $count | grep iperf3 | awk '{print $7}')
