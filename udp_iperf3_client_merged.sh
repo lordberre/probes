cc3ba00da42a98fbe30b420b65e33ef6788affea
lordberre <jakerlock@gmail.com>
2017-07-12 17:18:14 +0200

    Upgrade to version 0.96.17

--- udp_iperf3_client_merged.sh
+++ udp_iperf3_client_merged.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 1.4
+# Version 1.45
 
 while true
 do
@@ -26,21 +26,21 @@ logtag=chprobe_iperf3udp
 count=$(( ( RANDOM % 9999 )  + 1 ))
 
 # Remote url stuff
-ip_url="http://project-mayhem.se/probes/ip-udp-beta.txt"
+ip_url="http://project-mayhem.se/probes/ip-udp.txt"
 urlz="curl -m 3 -s -o /dev/null -w \"%{http_code}\" \$ip_url"
 urlcheck=$(eval $urlz)
 
 # Use cached ip if remote server is not responding
-if [ $urlcheck -ne 200 ]; then target="$(cat /var/ip_udp-beta.txt)"
-        else target="$(curl -m 3 -s $ip_url)" && curl -m 3 -s -o /var/ip_udp-beta.txt $ip_url
+if [ $urlcheck -ne 200 ]; then target="$(cat /var/ip-udp.txt)"
+        else target="$(curl -m 3 -s $ip_url)" && curl -m 3 -s -o /var/ip-udp.txt $ip_url
 fi
 
 # Daemon settings
 if [ $direction = "upstream" ]; then
-udpdaemon="/usr/bin/iperf3 --client \$target -u -T \$direction -b \${arr[\$rand]} -t 60 -p \${portz[\$randport]} | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print \$1,\$6,\$8,\$10,\$13,\$14.\$15,\$16,\$17,\$18}' | tr -d '(%)|:' | logger -t iperf3udp[\$(echo \$count)] -p \$logfacility"
+udpdaemon="/usr/bin/iperf3 -u --client \$target -T \$direction -b \${arr[\$rand]} -t 60 -p \${portz[\$randport]} | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print \$1,\$6,\$8,\$10,\$13,\$14.\$15,\$16,\$17,\$18}' | tr -d '(%)|:' | logger -t iperf3udp[\$(echo \$count)] -p \$logfacility"
 
 elif [ $direction = "downstream" ]; then
-udpdaemon="/usr/bin/iperf3 --client \$target -u -T \$direction -R -b \${arr[\$rand]} -t 60 -p \${portz[\$randport]} | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print \$1,\$6,\$8,\$10,\$13,\$14.\$15,\$16,\$17,\$18}' | tr -d '(%)|:' | logger -t iperf3udp[\$(echo \$count)] -p \$logfacility"
+udpdaemon="/usr/bin/iperf3 -u --client \$target -T \$direction -R -b \${arr[\$rand]} -t 60 -p \${portz[\$randport]} | egrep 'iperf Done' -B 3 | egrep 0.00-60.00 | awk '{print \$1,\$6,\$8,\$10,\$13,\$14.\$15,\$16,\$17,\$18}' | tr -d '(%)|:' | logger -t iperf3udp[\$(echo \$count)] -p \$logfacility"
 	else echo 'No direction specified, exiting.' && exit 1
 fi
 
@@ -66,24 +66,27 @@ arr[2]="20m"
 rand=$[ $RANDOM % 3 ]
 
 # Randomize ports to connect to.
-portz[0]="5203"
-portz[1]="5204"
-portz[2]="5205"
-portz[3]="5206"
-portz[4]="5207"
-portz[5]="5208"
-portz[6]="5209"
-portz[7]="5210"
-randport=$[ $RANDOM % 8 ]
+portz[0]="5210"
+portz[1]="5215"
+portz[2]="5220"
+portz[3]="5225"
+randport=$[ $RANDOM % 4 ]
 
+# Only go into anti-collision loop if the bandwidth is 20m
+if [ ${arr[$rand]} == "20m" ]; then udp_anticollision=1
+else udp_anticollision=0
+fi
 
 # Run tests and avoid collisions
 while [ `pgrep -f 'bbk|iperf3|wrk' | wc -w` -ge 30 ];do kill $(pgrep -f "iperf3|bbk|wrk" | awk '{print $1}') && echo "[chprobe_error] We're overloaded with daemons, killing everything" | logger -p local5.err ; done
-        while [ `pgrep -f 'bbk|tcp_iperf3' | wc -w` -ge 1 ];do echo "[$logtag] bbk or iperf3tcp is running, waiting." | logger -p info && sleep 3;done
+	if [ $udp_anticollision -eq 1 ]; then
+        	while [ `pgrep -f 'bbk|tcp_iperf3|iperf3tcp' | wc -w` -ge 1 ];do echo "[$logtag] bbk or iperf3tcp is running, waiting." | logger -p info && sleep 3
+		done
+	fi
 case "$(pgrep -f "iperf3 --client" | wc -w)" in
 
 0)  echo "[$logtag] Let's see if we can start the udp daemon" | logger -p info
-    while iperf3 -c $target -p ${portz[$randport]} -t 1 | grep busy; do randport=$[ $RANDOM % 8 ] && sleep $[ ( $RANDOM % 10 ) + 3]s && echo "[$logtag] server is busy. We slept a bit, now rolling the dice for the port" | logger -p info;done
+    while iperf3 -c $target -p ${portz[$randport]} -t 1 | grep busy; do randport=$[ $RANDOM % 4 ] && sleep $[ ( $RANDOM % 10 ) + 3]s && echo "[$logtag] server is busy. We slept a bit, now rolling the dice for the port" | logger -p info;done
     echo "[$logtag] udp daemon started - $direction" | logger -p info
 	eval $udpdaemon &
 	if [ $iwdetect -gt 0 ]; then
