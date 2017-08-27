a02d3314a7ed7398c56663dd2a1ef7a9712c9ba3
lordberre <jakerlock@gmail.com>
2017-08-27 21:55:16 +0200

    started optimizing for remote configuration

--- upnpc-probe.sh
+++ upnpc-probe.sh
@@ -1,8 +1,15 @@
 # Break the firewall
 # Only works with IPv4
+
+# Load config file
+source /var/chprobe/$(hostname -d).cfg
+
 ip="$(hostname -I | awk '{print $1}')"
 echo -n "$(hostname -d) " > /home/chprobe/hostname-$(hostname -d) ;echo $(curl -s http://ping.eu | grep Your | awk '{print $4}' | tr -d '</b>') >> /home/chprobe/hostname-$(hostname -d)
+
+if [ $chprobe_upnp = "enable" ]; then
 upnpc -e ch-probe -a $ip 5001 5001 tcp 1800
 upnpc -e ch-probe -a $ip 5001 5001 udp 1800
-upnpc -e ch-probe -a $ip 34521 34521 tcp 1800
+upnpc -e ch-probe -a $ip 34521 34521 tcp $chprobe_upnp_timer
 upnpc -e ch-probe -a $ip 35631 35631 tcp 1800
+fi
