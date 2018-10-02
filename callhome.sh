b4ed2ab89a7d7de728b3f4ee59a5f5ae340e84c5
lordberre <jakerlock@gmail.com>
2018-10-02 20:28:32 +0200

    properly fixed network checks + reboot mechanism

--- callhome.sh
+++ callhome.sh
@@ -9,50 +9,73 @@ ENVFILE="/var/chprobe/chprobe_forcedown"
 connectivity=true
 chprobe_reboot=disable
 
-
 # Load config file
 probe="`cut -d "." -f 2 <<< $(hostname)`"
 source /var/chprobe/chprobe.cfg || probedir="/home/chprobe"
 source $ENVFILE
 
+# Use a different logtag for link events
+if [[ $1 = "dispatcher" ]]; then error_tag="chprobe_link_error"
+else error_tag="chprobe_network_error"
+fi
+
 # Restart NM function
 restart_nm () {
-systemctl restart NetworkManager && echo "[chprobe_network_error] Seems we don't have internet connectivity, restarting NetworkManager in case it's just us" | logger -p local5.err
+systemctl restart NetworkManager && echo "[$error_tag] Seems we don't have internet connectivity, restarting NetworkManager in case it's just us" | logger -p local5.err
 }
 
 # ProbeReboot function
 declare -i probe_uptime=$(printf "%.0f\n" `awk {'print $1'} /proc/uptime`)
 reboot_probe () {
-echo "[chprobe_network_error] Rebooting probe due to excessive network errors.." | logger -p local5.emerg
+echo "[$error_tag] Rebooting probe due to excessive network errors.." | logger -p local5.emerg
 reboot
+}
+
+set_state() {
+noconnectivity="$(ping -c 10 8.8.8.8 | grep 100% | wc -l)"
+if [ $noconnectivity -ge 1 ]; then connectivity=false; echo "[$error_tag] Got no errors, but not a single packet went through" | logger -p local5.err
+else ping -q -c 1 8.8.8.8
+        if [ $? -ge 2 ]; then connectivity=false; echo "[$error_tag] We don't have connectivity (error returned: $?)" | logger -p local5.err 
+        fi
+fi
 }
 
 connectivity_check () {
 # Check connectivity and try to recover if not
-noconnectivity="$(ping -c 10 8.8.8.8 | grep 100% | wc -l)"
 
 if [ $noloop -eq 0 ]; then
-	if [ $noconnectivity -ge 1 ]; then connectivity=false; echo "[chprobe_network_error] Got no errors, but not a single packet went through" | logger -p local5.err && restart_nm
-	else ping -q -c 1 8.8.8.8
-		if [ $? -ge 2 ]; then connectivity=false; echo "[chprobe_network_error] We don't have connectivity (error returned: $?)" | logger -p local5.err && restart_nm
-		fi
-	fi
-else 
-	if [ $chprobe_forcedown = false ] && [ $connectivity = false ]; then
-ifdown $default_if;sleep 5;ifup $default_if;echo "[chprobe_network_error] Forced $default_if down and up again.." | logger -p local5.err && echo "chprobe_forcedown=false" > $ENVFILE
-	fi
+    set_state
+    if    [ $connectivity = false ]; then
+         restart_nm
+    fi
+
+else
+    set_state
+    if [ $chprobe_forcedown = false ] && [ $connectivity = false ]; then
+        ifdown $default_if
+        sleep 5
+        ifup $default_if
+        echo "[$error_tag] Forced $default_if down and up again.." | logger -p local5.err && echo "chprobe_forcedown=false" > $ENVFILE
+    fi
 fi
+return 
 }
 
-# Check journal for entries before reloading NM
-noloop="$(tail -n 500 /var/log/messages | grep -c 'Stopping Network Manager')" && connectivity_check
+# Check journal for entries before checking connectivity
+noloop="$(journalctl -S -1h -p 3 --no-pager | grep -c 'restarting NetworkManager')"
+connectivity_check
 
 # If script is called by dispatcher, be more aggressive
+# connectivity_check will be called again, we'll have the connection status(connectivity var) from the first run
 if [[ $1 = "dispatcher" ]]; then connectivity_check && sleep 5
 	if [ $connectivity = false ];then
 	noloop=1 && connectivity_check
 	fi
-else connectivity_check
+else
+    if [ $connectivity = false ];then
+        sleep 5
+        connectivity_check
+    fi
 fi
 
 # Report if connectivity is OK and reset ifup/down variable
@@ -90,24 +113,29 @@ fi
 
 if [ $dns_check = true ] && [ $connectivity = true ]; then
 # Send heartbeats
-curl -m 3 -s http://project-mayhem.se --data-ascii DATA -A ${probe} &> /dev/null
-curl -L -m 3 --retry 2 -s http://88.198.46.60 | grep Your | awk '{print $4}' | tr -d '</b>' | sed -e "s/^/$(date "+%b %d %H:%M:%S") ${probe} chprobe_wanip[$(echo 9000]): $(cd $probedir && ls version-* | sed 's/\<version\>//g') /" | tr -s ' ' | tr -d '-' >> /var/log/chprobe_wanip.txt
-
-# Check criterias for rebooting if configured
-if [ $connectivity = false ] && [ $chprobe_reboot = enable ] && [ $probe_uptime -ge 2000 ]; then
-    declare -i reboot_condition=`journalctl -S -2h -p 3 --no-pager | grep -c 'chprobe_network_error'`
-    if [ -z ${callhome_interval+x} ]; then
-        if [ $reboot_condition -ge 12 ];then
-             reboot_probe
-    fi
-    else
-        interval_formula=`expr 90 \* 120 / 100 / $callhome_interval \* 2`
-        if [ $reboot_condition -ge $interval_formula ]; then
-            reboot_probe
-        fi
-    fi
-fi
+    curl -m 3 -s http://project-mayhem.se --data-ascii DATA -A ${probe} &> /dev/null
+    curl -L -m 3 --retry 2 -s http://88.198.46.60 | grep Your | awk '{print $4}' | tr -d '</b>' | sed -e "s/^/$(date "+%b %d %H:%M:%S") ${probe} chprobe_wanip[$(echo 9000]): $(cd $probedir && ls version-* | sed 's/\<version\>//g') /" | tr -s ' ' | tr -d '-' >> /var/log/chprobe_wanip.txt
 
 # Update Probemanager
-curl -m 3 --retry 2 -s -o ${probedir}probemanager $managerurl &> /dev/null && chmod +x ${probedir}probemanager &> /dev/null
+    curl -m 3 --retry 2 -s -o ${probedir}probemanager $managerurl &> /dev/null && chmod +x ${probedir}probemanager &> /dev/null
+fi
+
+# Check criterias for rebooting if configured
+# Re-evaluate the state since it might've started working again after the most recent if-event
+
+if [ $connectivity = false ] && [ $chprobe_reboot = enable ] && [ $probe_uptime -ge 2000 ]; then
+    set_state
+    if [ $connectivity = false ]; then
+        declare -i reboot_condition=`journalctl -S -2h -p 3 --no-pager | grep -c 'chprobe_network_error'`
+        if [ -z ${callhome_interval+x} ]; then
+            if [ $reboot_condition -ge 24 ];then
+                 reboot_probe
+        fi
+        else
+            interval_formula=`expr 90 \* 120 / 100 / $callhome_interval \* 4`
+            if [ $reboot_condition -ge $interval_formula ]; then
+                reboot_probe
+            fi
+        fi
+    fi
 fi
