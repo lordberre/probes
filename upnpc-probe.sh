8e16840836e52a81275577e653446b1c499c85cd
lordberre <jakerlock@gmail.com>
2019-01-30 21:30:38 +0100

    clean up

--- upnpc-probe.sh
+++ upnpc-probe.sh
@@ -1,17 +1,23 @@
+#!/bin/bash
 # Break the firewall
 # Only works with IPv4
 
+# Defaults
+chprobe_upnp=enable
+chprobe_upnp_timer=1800
+
 # Load configuration file
-probe="`cut -d "." -f 2 <<< $(hostname)`"
 chprobe_configfile="/var/chprobe/chprobe.cfg"
 source $chprobe_configfile
 
-ip="$(hostname -I | awk '{print $1}')"
-echo -n "${probe} " > /home/chprobe/hostname-${probe} ;echo $(curl -s http://ping.eu | grep Your | awk '{print $4}' | tr -d '</b>') >> /home/chprobe/hostname-${probe}
-
-if [ $chprobe_upnp = "enable" ]; then
-upnpc -e ch-probe -a $ip 5001 5001 tcp $chprobe_upnp_timer
-upnpc -e ch-probe -a $ip 5001 5001 udp $chprobe_upnp_timer
-upnpc -e ch-probe -a $ip 34521 34521 tcp $chprobe_upnp_timer
-upnpc -e ch-probe -a $ip 35631 35631 tcp $chprobe_upnp_timer
+if [[ $chprobe_upnp = "enable" ]]; then
+    ip="$(hostname -I | awk '{print $1}')"
+    upnpc -e ch-probe -a "$ip" 5001 5001 tcp "$chprobe_upnp_timer"
+    upnpc -e ch-probe -a "$ip" 5001 5001 udp "$chprobe_upnp_timer"
+    upnpc -e ch-probe -a "$ip" 34521 34521 tcp "$chprobe_upnp_timer"
+    upnpc -e ch-probe -a "$ip" 35631 35631 tcp "$chprobe_upnp_timer"
 fi
+
+# Always exit with status 0
+exit 0
+
